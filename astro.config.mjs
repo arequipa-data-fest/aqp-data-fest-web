cc6d93fc42deaa7580de31140aebc80bb85d1e31
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2026-06-22 09:58:47 -0500

    feat: configure cloudflare to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: { enabled: true },
+  }),
+
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
